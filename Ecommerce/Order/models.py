aad6334984790987bb91fbdd5c0509ed7390e486
NorahWalker <thanhthomas24092003@gmail.com>
2022-11-29 17:37:07 +0700

    Create OrderItem

---
+++ Ecommerce/Order/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def str(self):
+        return self.title
