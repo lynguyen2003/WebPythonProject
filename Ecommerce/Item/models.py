b4ec5f09846f24fcff3744aa1106c19f59733de3
Ly Nguyen <nly36524@gmail.com>
2022-11-29 16:59:07 +0700

    create item app

---
+++ Ecommerce/Item/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
