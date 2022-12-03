d74f4f49e5db371680882efa3517e34feb70010b
Ly Nguyen <nly36524@gmail.com>
2022-12-03 18:20:59 +0700

    fix remove item from cart functions

--- Ecommerce/core/urls.py
+++ Ecommerce/core/urls.py
@@ -4,6 +4,7 @@ from .views import (
     checkout,
     ItemDetailView,
     add_to_cart,
+    remove_from_cart,
     cart,
 )
 
@@ -15,5 +16,5 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('cart/', cart, name='cart'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
 ]
--- Ecommerce/core/views.py
+++ Ecommerce/core/views.py
@@ -79,22 +79,24 @@ def add_to_cart(request, slug):
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(
-        user=request.user, 
+        user=request.user,
         ordered=False
     )
     if order_qs.exists():
         order = order_qs[0]
         # check if the order item is in the order
         if order.items.filter(item__slug=item.slug).exists():
-            order_item= OrderItem.objects.filter(item=item)[0]
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
             order.items.remove(order_item)
             messages.info(request, "This item was removed from your cart.")
-            return redirect("core.product", slug=slug)
+            return redirect("core:product", slug=slug)
         else:
-            messages.info(request, "This item wasn't in your cart.")
-            return redirect("core.product", slug=slug)
-            
-    else: 
-        messages.info(request, "You don't have an active order.")
-        return redirect("core.product", slug=slug)
-    return redirect("core.product", slug=slug)
+            messages.info(request, "This item was not in your cart")
+            return redirect("core:product", slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:product", slug=slug)
