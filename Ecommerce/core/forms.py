925c1f825858d6642eee562f7fed9a509bdf9cfb
NorahWalker <thanhthomas24092003@gmail.com>
2022-12-14 17:47:41 +0700

    fix checkout front-end

--- Ecommerce/core/forms.py
+++ Ecommerce/core/forms.py
@@ -5,16 +5,12 @@ from django_countries.widgets import CountrySelectWidget
 
 PAYMENT_CHOICES = (
     ('S', 'Stripe'),
-    ('P', 'PayPal')
+    ('P', 'PayPal'),
 )
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': '1234 Main St'
-    }))
-    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
-        'placeholder': 'Apartment or suite'
-    }))
+    street_address = forms.CharField(widget=forms.TextInput(attrs={}))
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={}))
     country = CountryField(blank_label='(select country)').formfield(
         widget=CountrySelectWidget(attrs={
             'class': 'custom-select d-block w-100'
