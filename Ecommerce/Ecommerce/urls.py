e8b5f9a06b533bb85228a1199783e4c457ee5b72
thanhraiik <nguyenjapanthanh@gmail.com>
2022-12-24 13:55:52 +0700

    Discount final

--- Ecommerce/Ecommerce/urls.py
+++ Ecommerce/Ecommerce/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,9 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls', namespace='core')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Ecommerce/core/models.py
+++ Ecommerce/core/models.py
@@ -1,20 +1,33 @@
 from django.conf import settings
 from django.db import models
 from django.db.models import Sum
-from django.urls import reverse
+from django.shortcuts import reverse
 from django_countries.fields import CountryField
 
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2, blank=True, null=True)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1, blank=True, null=True)
     slug = models.SlugField()
     description = models.TextField()
+    image = models.ImageField()
 
     
-
     def __str__(self):
         return self.title
 
@@ -100,6 +113,8 @@ class Payment(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
 
 
 
